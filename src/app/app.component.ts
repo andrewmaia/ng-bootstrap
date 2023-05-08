85a3dd6eccd5613f5fe941192a7f5105ed63ef33
Andrew Santos Maia <andrew.maia@talkcomm.com.br>
2023-05-08 13:58:17 -0300

    Combo por damanda

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,15 @@
 import { Component } from '@angular/core';
 import { NgbModal, NgbOffcanvas } from '@ng-bootstrap/ng-bootstrap';
+import { debounceTime, distinctUntilChanged, map, Observable, OperatorFunction } from 'rxjs';
+import { FormsModule } from '@angular/forms';
+
+const nomes = [
+	'Andrew',
+	'Andre',
+	'Adrea',
+	'Bruno',
+	'Bruna'
+];
 
 @Component({
   selector: 'app-root',
@@ -7,6 +17,7 @@ import { NgbModal, NgbOffcanvas } from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+	public model: any;
 	public isCollapsed = false;
 	currentRate = 8;
 
@@ -19,4 +30,13 @@ export class AppComponent {
 	openOffcanvas(content:any) {
 		this.offcanvasService.open(content, { ariaLabelledBy: 'offcanvas-basic-title' });
 	}
+
+	search: OperatorFunction<string, readonly string[]> = (text$: Observable<string>) =>
+	text$.pipe(
+		debounceTime(200),
+		distinctUntilChanged(),
+		map((term) =>
+			term.length < 2 ? [] : nomes.filter((v) => v.toLowerCase().indexOf(term.toLowerCase()) > -1).slice(0, 10),
+		),
+	);
 }
